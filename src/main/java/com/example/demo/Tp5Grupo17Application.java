404e74972a10ed92675ad1340bd36ab8077e8eca
Jonatan Garcia <sacha.255555@gmail.com>
2022-05-05 14:35:15 -0300

    Inicializado el Proyecto, agregados los archivos footer.html y header.html sin contenido

---
+++ src/main/java/com/example/demo/Tp5Grupo17Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp5Grupo17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp5Grupo17Application.class, args);
+	}
+
+}
